6fe8981508300be7ce0b27a196a893e3d14a7cdd
unknown <frankbrunoc@gmail.com>
2020-04-22 07:52:00 -0300

    test coment

---
+++ Fundamentos/fundamento.js
@@ -0,0 +1,19 @@
+//Tipo um  de comentario
+console.log("linha 1")
+
+/*tipo 
+dois de 
+comentario
+*/
+console.log('Linha 2')
+
+/*
+*tipo
+*três de
+*comentario
+*/
+console.log('Linha 3')
+
+
+/////////////////////////////////////////////////////
+
